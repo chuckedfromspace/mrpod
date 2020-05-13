555cc943904b7ba930d79bbbaa1f8a4cca1647ab
Yin <Zhiyao.Yin@dlr.de>
2020-05-13 12:56:11 +0200

    initial commit

---
+++ mrpod/_version.py
@@ -0,0 +1,13 @@
+"""
+Module where the version is written.
+It is executed in setup.py and imported in modred/__init__.py.
+See:
+http://en.wikipedia.org/wiki/Software_versioning
+http://legacy.python.org/dev/peps/pep-0386/
+'a' or 'alpha' means alpha version (internal testing),
+'b' or 'beta' means beta version (external testing).
+Append with .postN for post-release updates.  This may be necessary for changes
+in packaging or documentation, as PyPI does not allow uploads of files with the
+same filename (which corresponds to the version number).
+"""
+__version__ = '0.0.1'
