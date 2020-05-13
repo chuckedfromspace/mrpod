1839775d5dfc1dd4821747aebe6b11a99db827bc
Yin <Zhiyao.Yin@dlr.de>
2020-05-13 12:56:48 +0200

    MAINT: modified the code structure

--- mrpod/__init__.py
+++ mrpod/__init__.py
@@ -1,6 +1,12 @@
 """
 MRPOD
 """
+from __future__ import division, print_function, absolute_import
+
 from .wavelet_transform import scale_to_frq, CompositeFilter, WaveletTransform
 
 from .modal_decomposition import mrpod_eigendecomp, mrpod_detail_bundle
+
+from ._version import __version__
+
+__all__ = [s for s in dir()]
