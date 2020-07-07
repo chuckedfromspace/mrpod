3d3f75085d4dfa08ae8d2bee2cb7a162a9044b93
Yin <Zhiyao.Yin@dlr.de>
2020-07-07 17:57:39 +0200

    MAINT: edited GitHub display

--- doc/conf.py
+++ doc/conf.py
@@ -36,8 +36,7 @@ extensions = [
         'numpydoc',
         'sphinx.ext.mathjax',
         'sphinx.ext.autodoc',
-        'sphinx.ext.doctest',
-        'sphinx.ext.githubpages']
+        'sphinx.ext.doctest']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
